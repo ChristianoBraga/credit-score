d03a5403fd9cb673ccb4977fa6e95b3f0ffb175e
Christiano Braga <cbraga@ic.uff.br>
2023-11-01 12:02:07 -0300

    - Change parameter modifier of `fireVerifyProof` of `contracts/VerifyProof.sol` from `memory` to `calldata` as `key` and `proof` are both immutable.

--- contracts/VerifyProof.sol
+++ contracts/VerifyProof.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 contract VerifyProof {
     event VerifyProof(address indexed de, string key, string proof);
 
-    function fireVerifyProof(string memory key, string memory proof) public {
+    function fireVerifyProof(string calldata key, string calldata proof) public {
         emit VerifyProof(msg.sender, key, proof);
     }
 }
